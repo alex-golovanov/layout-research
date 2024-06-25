127b73047bf022b4085276fe29108bfa3bbf3dc1
Alex Golovanov <alex@appsmith.com>
2024-06-25 22:56:09 +0300

    perf: tweak view

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -3,24 +3,26 @@ import { GridArea, ViewMode } from "../enums";
 import { useMemo } from "react";
 
 interface ViewConfig {
-  gridArea: GridArea;
+  gridArea?: GridArea;
   hidden?: boolean;
 }
 
 interface ViewProps<T extends ViewMode> {
   mode: T;
-  config: Record<T, ViewConfig>;
+  defaultArea: GridArea;
+  config?: Partial<Record<T, ViewConfig>>;
   children: React.ReactNode;
   style: Partial<React.CSSProperties>;
 }
 
 export function View<T extends ViewMode>({
   mode,
+  defaultArea,
   config,
   children,
   style,
 }: ViewProps<T>) {
-  const { gridArea, hidden } = config[mode];
+  const { gridArea = defaultArea, hidden = false } = config?.[mode] || {};
 
   const derivedStyle = useMemo(() => {
     const res: React.CSSProperties = { gridArea, ...(style || []) };
