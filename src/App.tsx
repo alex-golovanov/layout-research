0e4e8f74d078923a013df28c02220c1c19f000c4
Alex Golovanov <alex@appsmith.com>
2024-07-25 19:45:58 +0300

    feat: add cell component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { faker } from "@faker-js/faker";
 
 import { GridArea, ViewMode } from "./enums";
 import { AnimatedGridLayout, LayoutArea } from "./components";
@@ -36,9 +37,43 @@ const COLUMNS = {
 
 const COMMON_CELL_STYLES = {
   display: "grid",
-  placeContent: "center",
   height: "100%",
   width: "100%",
+  placeContent: "center",
+};
+
+const BACKGROUNDS: Partial<Record<GridArea, string>> = {
+  [GridArea.CANVAS]: "#FC8603",
+  [GridArea.EDITOR]: "#0379FC",
+  [GridArea.LEFT]: "#8B3DC2",
+  [GridArea.RIGHT]: "#74C23D",
+  [GridArea.SIDEBAR]: "#FFAA00",
+  [GridArea.HEADER]: "#F81407",
+};
+
+const Cell = ({ type }: { type: `${GridArea}` }) => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        width: "100%",
+        gridTemplateRows: "50px 1fr",
+        placeContent: "center",
+        background: BACKGROUNDS[type] || "pink",
+        padding: 24,
+      }}
+    >
+      <h4>{type}</h4>
+      <div>
+        {faker.lorem
+          .paragraphs(10, "|")
+          .split("|")
+          .map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 function App() {
@@ -49,7 +84,7 @@ function App() {
         display: "grid",
         placeContent: "center",
         height: "100vh",
-        background: "orange",
+        background: "#1ECBE1",
       }}
     >
       <div
@@ -69,12 +104,12 @@ function App() {
           }}
         >
           <button onClick={() => setMode(ViewMode.CODE_EDIT)}>CODE_EDIT</button>
+          <button onClick={() => setMode(ViewMode.CANVAS_EDIT)}>
+            CANVAS_EDIT
+          </button>
           <button onClick={() => setMode(ViewMode.PREVIEW)}>PREVIEW</button>
           <button onClick={() => setMode(ViewMode.SIDE_BY_SIDE)}>
             SIDE_BY_SIDE
-          </button>
-          <button onClick={() => setMode(ViewMode.CANVAS_EDIT)}>
-            CANVAS_EDIT
           </button>
         </div>
 
@@ -85,32 +120,39 @@ function App() {
           width="100vw"
         >
           <LayoutArea name={GridArea.HEADER}>
-            <div style={{ ...COMMON_CELL_STYLES, background: "green" }}>
-              HEADER
+            <div
+              style={{
+                ...COMMON_CELL_STYLES,
+                background: BACKGROUNDS[GridArea.HEADER],
+              }}
+            >
+              <h4>HEADER</h4>
             </div>
           </LayoutArea>
           <LayoutArea name={GridArea.SIDEBAR}>
-            <div style={{ ...COMMON_CELL_STYLES, background: "red" }}>SB</div>
+            <div
+              style={{
+                ...COMMON_CELL_STYLES,
+                background: BACKGROUNDS[GridArea.SIDEBAR],
+              }}
+            >
+              <h4>SB</h4>
+            </div>
           </LayoutArea>
           <LayoutArea name={GridArea.LEFT}>
-            <div style={{ ...COMMON_CELL_STYLES, background: "purple" }}>
-              LEFT
-            </div>
+            <Cell type={GridArea.LEFT} />
           </LayoutArea>
           <LayoutArea name={GridArea.EDITOR}>
-            <div style={{ ...COMMON_CELL_STYLES, background: "gold" }}>
-              EDITOR
-            </div>
+            <Cell type={GridArea.EDITOR} />
           </LayoutArea>
           <LayoutArea name={GridArea.CANVAS}>
-            <div style={{ ...COMMON_CELL_STYLES, background: "skyblue" }}>
-              CANVAS
-            </div>
+            <Cell type="CANVAS" />
           </LayoutArea>
           <LayoutArea name={GridArea.RIGHT}>
-            <div style={{ ...COMMON_CELL_STYLES, background: "blue" }}>
+            {/* <div style={{ ...COMMON_CELL_STYLES, background: "blue" }}>
               RIGHT
-            </div>
+            </div> */}
+            <Cell type={GridArea.RIGHT} />
           </LayoutArea>
         </AnimatedGridLayout>
       </div>
